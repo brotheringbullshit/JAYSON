0777b3cfdf5846d9fb991617a5902ee5dd446b82
brotheringbullshit <166644732+brotheringbullshit@users.noreply.github.com>
2024-11-14 20:11:46 -0300

    Update jayson.js

--- jayson.js
+++ jayson.js
@@ -1,175 +1,110 @@
 const fs = require('fs');
 
 class JaysonInterpreter {
-  constructor() {
+  constructor(file) {
+    this.file = file;
     this.variables = {};
+    this.functions = {};
   }
 
-  // Load and parse the JAYSON file
-  load(filePath) {
-    const fileContent = fs.readFileSync(filePath, 'utf8');
-    this.program = JSON.parse(fileContent);
-  }
-
-  // Interpret the program (the list of statements)
-  interpret() {
-    if (!this.program || !Array.isArray(this.program)) {
-      throw new Error('Invalid JAYSON file structure');
-    }
-
-    this.program.forEach(statement => {
-      this.execute(statement);
-    });
-  }
-
-  // Execute a single statement based on its key
-  execute(statement) {
-    for (const [key, value] of Object.entries(statement)) {
-      switch (key) {
-        case 'declare':
-          this.declare(value);
-          break;
-        case 'assign':
-          this.assign(value);
-          break;
-        case 'if':
-          this.ifStatement(value);
-          break;
-        case 'loop':
-          this.loopStatement(value);
-          break;
-        case 'print':
-          this.print(value);
-          break;
-        case 'add':
-          this.add(value);
-          break;
-        case 'subtract':
-          this.subtract(value);
-          break;
-        case 'greaterThan':
-          this.greaterThan(value);
-          break;
-        case 'lessThan':
-          this.lessThan(value);
-          break;
-        default:
-          console.log(`Unknown statement: ${key}`);
-          console.loo("please add the statement yourself, i am NOT adding it myself");
-      }
+  loadFile() {
+    try {
+      const data = fs.readFileSync(this.file, 'utf-8');
+      return JSON.parse(data);
+    } catch (error) {
+      console.error("Error reading the .jayson file:", error);
+      return null;
     }
   }
 
-  // Declare a variable
-  declare(declaration) {
-    const { var: variableName, value } = declaration;
-    this.variables[variableName] = value;
-  }
-
-  // Assign a new value to an existing variable
-  assign(assignment) {
-    const { var: variableName, value } = assignment;
-    if (this.variables.hasOwnProperty(variableName)) {
-      this.variables[variableName] = value;
-    } else {
-      console.log(`ERR: Variable ${variableName} not declared`);
+  evaluateOperation(operation) {
+    switch (operation.operation) {
+      case 'add':
+        return this.getValue(operation.left) + this.getValue(operation.right);
+      case 'subtract':
+        return this.getValue(operation.left) - this.getValue(operation.right);
+      case 'multiply':
+        return this.getValue(operation.left) * this.getValue(operation.right);
+      case 'divide':
+        return this.getValue(operation.left) / this.getValue(operation.right);
+      case 'equals':
+        return this.getValue(operation.left) === this.getValue(operation.right);
+      case 'greater_than':
+        return this.getValue(operation.left) > this.getValue(operation.right);
+      case 'less_than':
+        return this.getValue(operation.left) < this.getValue(operation.right);
+      case 'and':
+        return this.getValue(operation.left) && this.getValue(operation.right);
+      case 'or':
+        return this.getValue(operation.left) || this.getValue(operation.right);
+      case 'not':
+        return !this.getValue(operation.value);
+      case 'print':
+        console.log(this.getValue(operation.value));
+        break;
+      case 'if':
+        if (this.getValue(operation.condition)) {
+          operation.then.forEach((op) => this.evaluateOperation(op));
+        }
+        break;
+      case 'while':
+        while (this.getValue(operation.condition)) {
+          operation.body.forEach((op) => this.evaluateOperation(op));
+        }
+        break;
+      case 'call':
+        this.executeFunction(operation.function, operation.arguments);
+        break;
+      default:
+        throw new Error(`Unknown operation: ${operation.operation}`);
     }
   }
 
-  // Handle 'if' statements
-  ifStatement({ condition, then }) {
-    if (this.evaluateCondition(condition)) {
-      then.forEach(statement => {
-        this.execute(statement);
-      });
-    }
+  getValue(reference) {
+    return typeof reference === 'string' ? this.variables[reference] : reference;
   }
 
-  // Handle conditions (like equality)
-  evaluateCondition(condition) {
-    if (condition.eq) {
-      const [varName, value] = condition.eq;
-      return this.variables[varName] === value;
-    }
-    if (condition.greaterThan) {
-      const [varName, value] = condition.greaterThan;
-      return this.variables[varName] > value;
-    }
-    if (condition.lessThan) {
-      const [varName, value] = condition.lessThan;
-      return this.variables[varName] < value;
-    }
-    return false;
+  defineVariables(vars) {
+    this.variables = { ...this.variables, ...vars };
   }
 
-  // Handle addition
-  add({ var: variableName, value }) {
-    if (this.variables.hasOwnProperty(variableName)) {
-      this.variables[variableName] += value;
-    } else {
-      console.log(`ERR: Variable ${variableName} not declared`);
-    }
+  defineFunctions(funcs) {
+    this.functions = funcs;
   }
 
-  // Handle subtraction
-  subtract({ var: variableName, value }) {
-    if (this.variables.hasOwnProperty(variableName)) {
-      this.variables[variableName] -= value;
-    } else {
-      console.log(`ERR: Variable ${variableName} not declared`);
-    }
+  executeFunction(funcName, args) {
+    const func = this.functions[funcName];
+    if (!func) throw new Error(`Undefined function: ${funcName}`);
+    
+    const localVars = func.parameters.reduce((acc, param, index) => {
+      acc[param] = this.getValue(args[index]);
+      return acc;
+    }, {});
+
+    const originalVariables = { ...this.variables };
+    this.variables = { ...this.variables, ...localVars };
+
+    func.body.forEach((operation) => this.evaluateOperation(operation));
+
+    this.variables = originalVariables;
   }
 
-  // Handle loops
-  loopStatement({ times, body }) {
-    for (let i = 0; i < times; i++) {
-      body.forEach(statement => {
-        this.execute(statement);
-      });
-    }
-  }
+  run() {
+    const data = this.loadFile();
+    if (!data || !data.program) return;
 
-  // Handle print statements
-  print(message) {
-    if (Array.isArray(message)) {
-      console.log(...message);
-    } else {
-      console.log(message);
-    }
-  }
+    this.defineVariables(data.program.variables || {});
+    this.defineFunctions(data.program.functions || {});
 
-  // Handle greaterThan condition
-  greaterThan(value) {
-    const [varName, checkValue] = value;
-    return this.variables[varName] > checkValue;
-  }
-
-  // Handle lessThan condition
-  lessThan(value) {
-    const [varName, checkValue] = value;
-    return this.variables[varName] < checkValue;
+    (data.program.main || []).forEach((operation) => this.evaluateOperation(operation));
   }
 }
 
-// Main function to run the interpreter
-function runJaysonProgram(filePath) {
-  const interpreter = new JaysonInterpreter();
-  interpreter.load(filePath);
-  interpreter.interpret();
-}
-
-// Check if a file path is provided in the arguments
-const args = process.argv.slice(2);
-if (args.length === 0) {
-  console.log("provide a .jayson file. or it aint gonna work");
+const fileName = process.argv[2];
+if (!fileName.endsWith('.jayson')) {
+  console.error("Please provide a .jayson file to execute.");
   process.exit(1);
 }
 
-const filePath = args[0];
-if (!filePath.endsWith('.jayson')) {
-  console.log("The provided file must have a .jayson extension. please don't tell me yor dumb ahh was tryna run a .json file...");
-  process.exit(1);
-}
-
-// Run the program
-runJaysonProgram(filePath);
+const interpreter = new JaysonInterpreter(fileName);
+interpreter.run();
